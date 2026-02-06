b47ed350554392ad9ba1152163297638000f067a
Manish Mathur <madiv@Manishs-MacBook-Pro.local>
2026-02-06 19:11:58 +0530

    🧱 Add placeholder Python, C and C++ solutions for environment setup  - Introduces stub implementations for Python and C/C++ solutions  - Allows early validation of project structure and test discovery  - Confirms C/C++ build, bindings, and runtime integration  - Logic is intentionally minimal and will be implemented later

--- source/python/solution.py
+++ source/python/solution.py
@@ -1,5 +1,5 @@
 from typing import List
 
 class Solutio(object):
-    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+    def py_canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
         return -1
